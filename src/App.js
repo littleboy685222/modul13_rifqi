ff0e70b830c4229cc7d7719f374056ea15f5f52e
littleboy685222 <91310558+littleboy685222@users.noreply.github.com>
2022-10-30 20:53:34 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import './App.css';
+import Title from './Title';
+import Tabel from './Tabel';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+      <Tabel/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+function Footer(){
+    return(
+        <div className="footer">
+            <p>Muhammad Rifqi Fauzan</p>
+            <p>320200401017</p>
+            <p>&#169; 2022 by Netlab Bookstore</p>
+        </div>
+    )
+}
+
+export default Footer;
